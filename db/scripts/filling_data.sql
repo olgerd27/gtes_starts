440bf53fc81a8f219976d272405fc50be307c074
Oleg <olgerd27@gmail.com>
2015-07-15 11:30:05 +0300

    Test data loading from the files to the DB.
    Signed-off-by: Oleg <olgerd27@gmail.com>

---
+++ db/scripts/filling_data.sql
@@ -0,0 +1,11 @@
+LOAD DATA INFILE 'D:/work/Starts_data_base/project/gtes_starts/db/scripts/data__graphs_parameters_type.dat'
+REPLACE
+INTO TABLE gtes_starts.graphs_parameters_type
+CHARACTER SET cp1251;
+SELECT * FROM gtes_starts.graphs_parameters_type;
+
+LOAD DATA INFILE 'D:/work/Starts_data_base/project/gtes_starts/db/scripts/data__graphs_parameters_values.dat'
+REPLACE
+INTO TABLE gtes_starts.graphs_parameters_values
+CHARACTER SET cp1251;
+SELECT par_values FROM gtes_starts.graphs_parameters_values;
